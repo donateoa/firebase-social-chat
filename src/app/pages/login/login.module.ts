391baed15e7d3fdb6d3c18ed8906a37f7c226e74
donateoa <alessandro.donateo@gmail.com>
2019-02-05 12:13:54 +0100

    Add Login Page

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,20 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {IonicModule} from '@ionic/angular';
+import {FirebaseUIModule} from 'firebaseui-angular';
+
+import {LoginPage} from './login.page';
+
+const routes: Routes = [{path: '', component: LoginPage}];
+
+@NgModule({
+  imports: [
+    CommonModule, FormsModule, IonicModule, FirebaseUIModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {
+}
