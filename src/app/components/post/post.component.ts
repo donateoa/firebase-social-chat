d693e5b3c70834a8985d438b0bf0c7ccddacc800
donateoa <alessandro.donateo@gmail.com>
2019-02-20 11:33:05 +0100

    Add Toogle Comments

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {IonButton} from '@ionic/angular';
+import {Observable, Subject, combineLatest, fromEvent} from 'rxjs';
+import {map, scan, startWith} from 'rxjs/operators';
 import {PostComment} from 'src/app/model/post-comment.model';
 import {Post} from 'src/app/model/post.model';
 import {Principal} from 'src/app/services/Principal';
@@ -14,14 +16,35 @@ import {PostCommentsService} from './post-comments.service';
 })
 export class PostComponent implements OnInit {
   @Input('postId') _postId: string;
+
   post$: Observable<Post>;
   comment: string;
   comments$: Observable<PostComment[]>;
+  // handle toogle button show/hide more detail
+  clicks = new Subject<Event>();
+  toggleState = currentState => !currentState;
+  buttonInitialState = false;
+
+  clicks$ = this.clicks.asObservable()
+                .pipe(scan(this.toggleState, this.buttonInitialState))
+                .pipe(startWith(this.buttonInitialState));
+
+  // define a stream with combineLatest to show more or less comments
+  list$: Observable<PostComment[]>;
+
   @Input()
   set postId(postId: any) {
     this._postId = postId;
     this.post$ = this.postService.find(this._postId);
-    this.comments$ = this.postCommentsService.onSnapshot(this._postId);
+    this.comments$ = this.postCommentsService.onSnapshot(this._postId)
+                         .pipe(map(
+                             comments => comments.sort(
+                                 (a, b) => a.creationDate - b.creationDate)));
+    this.list$ = combineLatest(this.comments$, this.clicks$)
+                     .pipe(map(([comments, toggleState]) => {
+                       console.log(comments);
+                       return (toggleState ? comments : comments.slice(-3));
+                     }));
   }
   get postId(){return this._postId};
 
@@ -29,7 +52,13 @@ export class PostComponent implements OnInit {
       private postService: PostService,
       private postCommentsService: PostCommentsService,
       private principal: Principal) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.clicks$.subscribe(a => console.log('clicked', a));
+    this.comments$.subscribe(a => console.log('comments', a));
+    this.list$.subscribe(a => console.log('list', a));
+  }
+
+  toggleCommentClick(event: Event) { this.clicks.next(event); }
   addComment() {
     console.log(this.comment);
     const data: PostComment =
